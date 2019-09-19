b1f986e3f2893ef32362fcbe8bacea8c2d61a688
javierprado2000 <40152350+javierprado2000@users.noreply.github.com>
2019-09-18 19:42:24 -0500

    Update main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
+int main (){
+    int x;
+    int y;
+    cout<<"Que sona desea comprar, Super Vip (1), Vip (2), Preferencial (3), General (4): \n";
+    cin>> x;
+    cout<<"Si eres cliente claro, poner 1, de lo contrario poner 0:\n ";
+    cin>> y;
+    if (x==1)
+        if (y==1)
+            cout<<" Debe pagar 212 - 20% \n";
+        if (y==0)
+            cout<<" Debe pagar 212  \n";
+    if (x==2)
+        if (y==1)
+            cout<<" Debe pagar 170 - 20% \n";
+        if (y==0)
+            cout<<" Debe pagar 170  \n";
 
-int main() {
+    if (x==3)
+        if (y==1)
+            cout<<" Debe pagar 136 - 20% \n";
+        if (y==0)
+            cout<<" Debe pagar 136  \n";
+    if (x==4)
+        if (y==1)
+            cout<<" Debe pagar 59 - 20% \n";
+         if (y==0)
+            cout<<" Debe pagar 59  \n";
 
-
-// ToDO
-
-  return 0;
+     return 0;
 }
