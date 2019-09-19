29a66c3246014f711070cdddf0ee5223337538ad
javierprado2000 <40152350+javierprado2000@users.noreply.github.com>
2019-09-18 19:43:34 -0500

    Update main.cpp

--- ejercicio4/main.cpp
+++ ejercicio4/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
+int main (){
+ string x;
+ int y;
+ cout<<"Dime la letra: ";
+ cin>>x;
+ cout<<"Dime el numero: ";
+ cin>>y;
+ if (x=="a"||"c"||"e"||"g")
+     if (y%2==0& 1<=y<=8)
+         cout<<"Es negro";
+     if (!y%2==0& 1<=y<=8)
+         cout<<"Es blanco";
+     else
+         cout<<"el numero no es valido";
+ if (x=="b"||"d"||"f"||"h")
+    if (y%2==0& 1<=y<=8)
+            cout<<"Es blanco";
+    if (!y%2==0& 1<=y<=8)
+        cout<<"Es negro";
+    else
+        cout<<"el numero no es valido";
 
-int main() {
-
-entero numero;
-caracter crt;
-
-cout << "ingrese el caracter:";
-cin >> crt;
-cout << "\ningrese el numero:";
-cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
 }
